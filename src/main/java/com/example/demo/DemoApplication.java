140f12ef5327be4222f505596f03abc2034d376f
任铭君 <16253504+renmingjun@user.noreply.gitee.com>
2025-11-11 22:58:24 +0800

    Initial commit for EmojiChecker project

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+// 这是一个 Spring Boot 应用的启动类，运行该类的main即可启动SpringBoot项目
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
